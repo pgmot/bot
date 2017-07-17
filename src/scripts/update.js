64f419ff431ffa948ef72ab0ca61702e4ee6b86a
mot <mot.contact.address.pc@gmail.com>
2017-07-18 00:21:43 +0900

    self update

---
+++ src/scripts/update.js
@@ -0,0 +1,27 @@
+// Description:
+//   update
+// Commands:
+//   hubot update - update
+
+const child_process = require('child_process');
+const dumpError = require('../util').dumpError;
+
+module.exports = (robot => {
+  robot.respond(/update/i, msg => {
+    child_process.exec("git pull origin master", (error, stdout, stderr) => {
+      if(error){
+        dumpError(msg, error, stdout, stderr);
+        return;
+      }
+      msg.send('git pulled');
+      msg.send(`
+        \`\`\`
+        ${stdout}
+        \`\`\`
+      `);
+
+      msg.send('Good bye');
+      setTimeout(() => process.exit(0), 1500);
+    });
+  });
+});
