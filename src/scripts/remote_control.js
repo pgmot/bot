2416f66e57e4418bcc3a08b8ccb485b84b1d6d16
mot <mot.contact.address.pc@gmail.com>
2017-11-05 07:07:58 +0900

    add hdmi sw

--- src/scripts/remote_control.js
+++ src/scripts/remote_control.js
@@ -3,28 +3,36 @@
 // Commands:
 //   hubot aircon on - aircon on (cold, 25C, power level 2)
 //   hubot aircon off - aircon off
+//   hubot hdmi no1 - switch no1 (Fire TV)
+//   hubot hdmi no2 - switch no2 (switch)
 
 const child_process = require('child_process');
 const dumpError = require('../util').dumpError;
 
+function sendIR(msg, device, command){
+  child_process.exec(`irsend SEND_ONCE ${device} ${command}`, (error, stdout, stderr) => {
+    if(error){
+      dumpError(msg, error, stdout, stderr);
+      return;
+    }
+    msg.send(`irsend ${device} ${command} done`);
+  });
+}
+
 module.exports = (robot => {
   robot.respond(/aircon on/i, msg => {
-    child_process.exec("irsend SEND_ONCE aircon on", (error, stdout, stderr) => {
-      if(error){
-        dumpError(msg, error, stdout, stderr);
-        return;
-      }
-      msg.send("irsend aircon on done");
-    });
+    sendIR(msg, 'aircon', 'on');
   });
 
   robot.respond(/aircon off/i, msg => {
-    child_process.exec("irsend SEND_ONCE aircon off", (error, stdout, stderr) => {
-      if(error){
-        dumpError(msg, error, stdout, stderr);
-        return;
-      }
-      msg.send("irsend aircon off done");
-    });
+    sendIR(msg, 'aircon', 'off');
   });
+
+  robot.respond(/hdmi no1/i, msg => {
+    sendIR(msg, 'hdmi_sw', 'no1');
+  })
+
+  robot.respond(/hdmi no2/i, msg => {
+    sendIR(msg, 'hdmi_sw', 'no2');
+  })
 });
